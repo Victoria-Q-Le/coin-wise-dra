362f119948ade1296d185383852c20d315dfdfd3
Victoria Le <leqvictoria@gmail.com>
2023-01-18 17:10:04 -0500

    Header Component linked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+      </div>
+    </BrowserRouter>
   );
 }
 
