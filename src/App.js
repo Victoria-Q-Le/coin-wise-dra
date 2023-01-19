8e5a34dcf6cd2893b21d5a3625536307fb814bf7
Victoria Le <leqvictoria@gmail.com>
2023-01-19 14:50:31 -0500

    HomePage component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header'
+import HomePage from './Pages/HomePage'
+import CoinPage from './Pages/CoinPage'
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
     <BrowserRouter>
       <div>
         <Header />
+        <Route path='/' component={HomePage} />
+        <Route path='/coins/:id' component={CoinPage} />
       </div>
     </BrowserRouter>
   );
